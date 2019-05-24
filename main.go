8e9b91152ff343a86b5580dd748b3c6170ffec4b
g-harel <gabrielj.harel@gmail.com>
2019-05-23 20:06:09 -0400

    implement page to display package versions

--- main.go
+++ main.go
@@ -6,21 +6,50 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sort"
+	"strings"
 
 	"github.com/g-harel/rejstry/internal/middleware"
+	"github.com/g-harel/rejstry/internal/registry"
+	"github.com/g-harel/rejstry/internal/semver"
 	"github.com/g-harel/rejstry/server/api"
 )
 
 func pageVersions(w http.ResponseWriter, r *http.Request) {
+	parts := strings.Split(r.URL.Path, "/")
+	if parts[2] == "" {
+		http.NotFound(w, r)
+		return
+	}
+
 	tmpl, err := template.ParseFiles(
 		"templates/layout.html",
 		"templates/pages/versions.html",
 	)
 	if err != nil {
+		// TODO
 		panic(err)
 	}
 
-	err = tmpl.Execute(w, nil)
+	versions, latest, err := registry.PackageVersions("registry.npmjs.com", parts[2])
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		log.Printf("ERROR fetch package versions: %v", err)
+		return
+	}
+	sort.Sort(semver.Sort(versions))
+
+	context := &struct {
+		Package  string
+		Versions []string
+		Latest   string
+	}{
+		Package:  parts[2],
+		Versions: versions,
+		Latest:   latest,
+	}
+
+	err = tmpl.Execute(w, context)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
@@ -34,7 +63,7 @@ func main() {
 	http.HandleFunc("/api/v1/versions", middleware.Log(api.V1Versions))
 
 	// Rendered templates.
-	http.HandleFunc("/versions", middleware.Log(pageVersions))
+	http.HandleFunc("/package/", middleware.Log(pageVersions))
 
 	// Static files.
 	static := http.StripPrefix("/static/", http.FileServer(http.Dir("static")))
